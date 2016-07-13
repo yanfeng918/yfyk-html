7741e90df6004048bdcffc4a63c51af63fac3d31
yanfeng <yanfeng>
2016-07-13 19:22:11 +0800

    主页面修改

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,30 +1,32 @@
 $(function(){
     $('.recharge').on('click', function(){
-        layer.open({
-            id:"layer-charge",
-            type: 2,
-            title: '账户充值',
-            maxmin: true,
-            shadeClose: true, //点击遮罩关闭层
-            //area : ['800px' , '520px'],
-            area : ['100%' , '420px'],
-            content: 'finance/rechargeV2.html'
-            ,success: function(layero, index){
-                console.log(layero, index);
-                $(layero).addClass("col-md-6 col-sm-10 col-xs-12")
-            }
-        });
+        layer.msg("尽请期待！")
+        //layer.open({
+        //    id:"layer-charge",
+        //    type: 2,
+        //    title: '账户充值',
+        //    maxmin: true,
+        //    shadeClose: true, //点击遮罩关闭层
+        //    //area : ['800px' , '520px'],
+        //    area : ['100%' , '420px'],
+        //    content: 'finance/rechargeV2.html'
+        //    ,success: function(layero, index){
+        //        console.log(layero, index);
+        //        $(layero).addClass("col-md-6 col-sm-10 col-xs-12")
+        //    }
+        //});
     });
 
     $('.rechargeAll').on('click', function(){
-        layer.open({
-            type: 2,
-            title: '充值记录',
-            maxmin: true,
-            shadeClose: true, //点击遮罩关闭层
-            //area : ['800px' , '520px'],
-            content: 'finance/recharge_all.html'
-        });
+        layer.msg("尽请期待，马上上线！")
+        //layer.open({
+        //    type: 2,
+        //    title: '充值记录',
+        //    maxmin: true,
+        //    shadeClose: true, //点击遮罩关闭层
+        //    //area : ['800px' , '520px'],
+        //    content: 'finance/recharge_all.html'
+        //});
     });
 
 
